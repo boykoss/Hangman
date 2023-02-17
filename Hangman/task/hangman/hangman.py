b65f349814f7f88bd9f160332cdf63c069ce0aae
My Name <ccrpted@gmail.com>
2023-02-17 11:11:08 +0300

    3/8 Objectives Create the following list of words: python, java, swift, javascript; Program the game to choose a word from the list at random (you can use the random library for that). You can enter more words, but let's stick to these four for now.

--- Hangman/task/hangman/hangman.py
+++ Hangman/task/hangman/hangman.py
@@ -3,6 +3,12 @@ print("H A N G M A N")
 print("Guess the word:")
 l = ["python", "java", "swift", "javascript"]
 word = random.choice(l)
+answer = input()
+if (answer == word):
+    print("You survived!")
+else:
+    print("You lost!")
+    print(answer)
+    print(word)
 
-print(word)
 
